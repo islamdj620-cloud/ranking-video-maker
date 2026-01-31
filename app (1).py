64fb15fa6e398fed0ce1a0325e04a71123f88c90
islamdj620-cloud <islamdj620@gmail.com>
2026-01-31 07:42:30 +0100

    Add default fontsize to render_text_image function

--- app (1).py
+++ app (1).py
@@ -23,4 +23,12 @@ VIDEO_SIZE = (720, 1280)
 FPS = 24
 
 
-def render_text_image(text, size, font_path=None, fontsize=
+def render_text_image(text, size, font_path=None, fontsize=70):
+    image = Image.new('RGBA', size, (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    try:
+        font = ImageFont.truetype(font_path, fontsize) if font_path else ImageFont.load_default()
+    except:
+        font = ImageFont.load_default()
+    draw.text((size[0]//2, size[1]//2), text, font=font, fill="white", anchor="mm")
+    return image
